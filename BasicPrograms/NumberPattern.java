baa4caa26590078e8f8bf3d7fc79d1ecca98641b
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-08 15:26:46 +0530

    Number Pattern 02

--- BasicPrograms/NumberPattern.java
+++ BasicPrograms/NumberPattern.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class NumberPattern {
    
 //Pattern 01    
-    public static void main(String[] args) {
+/*     
+    public static void main(String[] args) 
+    {
         
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Rows : ");
@@ -16,11 +18,33 @@ public class NumberPattern {
                 System.out.print((j+1) + " ");
             }
             System.out.println();
+        }        
+    }
+ */
+//-------------------------------------------------------------------
+
+//Pattern 02   
+
+    public static void main(String[] args) 
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows : ");
+        int rows = sc.nextInt();
+        int count=1;
+
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(count +" ");
+                count++;
+            }
+            System.out.println();
         } 
 
-
-        
     }
+
 
 
 }
