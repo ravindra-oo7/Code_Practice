c7446796b583526fcb1cef785eafdba0c50957d4
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-08 15:05:00 +0530

    Diamond star Pattern

--- BasicPrograms/StarPattern1.java
+++ BasicPrograms/StarPattern1.java
@@ -462,32 +462,50 @@ public class StarPattern1 {
 
     public static void main(String[] args) 
     {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);  
+        System.out.print("Enter the number of rows you want to print: ");  
+        int rows = sc.nextInt(); 
+    
+        for (int i=1; i<= rows ; i++)   
+        {  
+            for (int j = rows; j > i ; j--)   
+            {  
+                System.out.print(" ");  
+            }  
+            System.out.print("*");  
 
-        System.out.print("Enter number : ");
-        final int base = sc.nextInt();
-        int rows = (base*2)-1;
-
-        for(int i=0;i<rows;i++)
-        {
-            for(int j=0;j<=rows;j++)
-            {
-                if(j==base-i || j==base+i)
-                {
-                    System.out.print("*");
-                }
-                else
-                {
-                    System.out.print(" ");
-                }
+            for (int k = 1; k < 2*(i -1) ;k++)   
+            {   
+                System.out.print(" ");   
             }
-            System.out.println();
 
-            
-
-        }
-    }
-
+            if( i==1)   
+            {   
+                System.out.println("");  
+            }  
+            else  
+            {  
+                System.out.println("*");   
+            }  
+        }   
+        for (int i=rows-1; i>= 1 ; i--)  
+        {  
+            for (int j = rows; j > i ; j--)   
+            {  
+                System.out.print(" ");  
+            }  
+                System.out.print("*");  
+            for (int k = 1; k < 2*(i -1) ;k++)   
+            {  
+                System.out.print(" ");  
+            }  
+            if(i==1)  
+                System.out.println("");  
+            else  
+                System.out.println("*");  
+        }  
+    }  
+      
 
 
 } 
