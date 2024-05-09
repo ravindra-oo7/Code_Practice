3f77ec4e7b4e72e58f0144b3ee57703d30281163
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-09 11:37:51 +0530

    Number Pattern 03

--- BasicPrograms/NumberPattern.java
+++ BasicPrograms/NumberPattern.java
@@ -50,10 +50,27 @@ public class NumberPattern {
 
 //Pattern 03
 
-    public static void main(String[] args) {
+    public static void main(String[] args)   
+    {  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows : ");
+        int rows = sc.nextInt();
+        int count=1;
+       
 
-        
+        for (int i = 0; i < rows; i++)   
+        {  
+            int number = 1;  
+            //For spaces
+            System.out.printf("%" + (rows - i) * 2 + "s", "");  
 
-    }
+            for (int j = 0; j <= i; j++)   
+            {  
+                System.out.printf("%4d", number);  
+                number = number * (i - j) / (j + 1);  
+            }  
+            System.out.println();  
+        }  
+    }  
 
 }
