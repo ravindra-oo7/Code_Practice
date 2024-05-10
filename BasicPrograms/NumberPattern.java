9d7c35c157d0bd6f3f1112f58e11a35d78cc76a3
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-10 12:40:40 +0530

    Number Pattern 14

--- BasicPrograms/NumberPattern.java
+++ BasicPrograms/NumberPattern.java
@@ -336,7 +336,7 @@ public static void main(String[] args)
 
  //---------------------------------------------------------------------------------------
 //Pattern 13
- 
+/*  
     public static void main(String[] args)   
     {  
         Scanner sc = new Scanner(System.in);
@@ -353,5 +353,25 @@ public static void main(String[] args)
         }
 
     }
+ */  
+//---------------------------------------------------------------------------------------
+//Pattern 14
+ 
+public static void main(String[] args)   
+{  
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Rows : ");
+    int rows = sc.nextInt();
+
+    for(int i=rows;i>0;i--)
+    {
+        for(int j=i;j>0;j--)
+        {
+            System.out.print(j +" ");
+        }
+        System.out.println();
+    }
+
+}  
 
 }
