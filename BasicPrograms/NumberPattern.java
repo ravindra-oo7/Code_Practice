b7a3f675ad1a8de7ddfe407b8eddaa67cb45d992
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-10 11:21:16 +0530

    Number Pattern 11

--- BasicPrograms/NumberPattern.java
+++ BasicPrograms/NumberPattern.java
@@ -248,7 +248,7 @@ public static void main(String[] args)
 
 //---------------------------------------------------------------------------------------
 //Pattern 10
- 
+/*  
     public static void main(String[] args)   
     {  
         Scanner sc = new Scanner(System.in);
@@ -282,9 +282,34 @@ public static void main(String[] args)
 
             System.out.println();
         }
+    }
+ */   
+ //---------------------------------------------------------------------------------------
+//Pattern 11
+ 
+    public static void main(String[] args)   
+    {  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows : ");
+        int rows = sc.nextInt();
 
+        for (int i=1;i<=rows;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        } 
 
-
+        for(int i=rows;i>0;i--)
+        {
+            for(int j=1;j<i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
     }
 
 
