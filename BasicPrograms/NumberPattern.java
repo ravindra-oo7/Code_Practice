56da7b6093c40263f761d291aa626928fb0456f5
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-10 18:14:28 +0530

    Number Pattern 17

--- BasicPrograms/NumberPattern.java
+++ BasicPrograms/NumberPattern.java
@@ -408,7 +408,7 @@ public static void main(String[] args)
 
 //---------------------------------------------------------------------------------------
 //Pattern 16
- 
+/*  
     public static void main(String[] args)   
     {  
         Scanner sc = new Scanner(System.in);
@@ -435,6 +435,41 @@ public static void main(String[] args)
         }
 
     }
+ */
+//---------------------------------------------------------------------------------------
+//Pattern 17
+ 
+    public static void main(String[] args)   
+    {  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows : ");
+        int rows = sc.nextInt();
 
+        for(int i=1;i<=rows;i++)
+        {
+            for(int j=1;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=i;k<=rows;k++)
+            {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+
+        for(int i=1;i<rows;i++)
+        {
+            for(int j=rows-1;j>i;j--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=rows-i;j<=rows;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 
 }
