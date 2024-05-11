b240e32d35d67c0029c8ac72c006fc571ded6674
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-11 11:58:16 +0530

    Number Pattern 20

--- BasicPrograms/NumberPattern.java
+++ BasicPrograms/NumberPattern.java
@@ -500,7 +500,7 @@ public static void main(String[] args)
 */
 //---------------------------------------------------------------------------------------
 //Pattern 19
- 
+/*  
     public static void main(String[] args)   
     {  
         Scanner sc = new Scanner(System.in);
@@ -521,6 +521,30 @@ public static void main(String[] args)
                 count--;
             }
             System.out.println();
+        }
+    }
+ */   
+//---------------------------------------------------------------------------------------
+//Pattern 19
+
+    public static void main(String[] args)   
+    {  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows : ");
+        int rows = sc.nextInt();
+
+        for(int i=1;i<=rows;i++)
+        {
+            for(int j=i;j<=rows;j++)
+            {
+                System.out.print(j+" ");
+            }
+            for(int j=1;j<i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+
         }
     }
 
