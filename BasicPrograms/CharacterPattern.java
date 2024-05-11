059005bb25163be4068981db3cfd065e3fb4d238
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-11 19:19:31 +0530

    Character Pattern 05

--- BasicPrograms/CharacterPattern.java
+++ BasicPrograms/CharacterPattern.java
@@ -83,7 +83,7 @@ public class CharacterPattern {
  */
 //---------------------------------------------------------------------------------------
 //Pattern 04
-
+/* 
      public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -106,5 +106,52 @@ public class CharacterPattern {
         }
 
      }
+ */     
+//---------------------------------------------------------------------------------------
+//Pattern 05
+
+    public static void main(String[] args) {
+            
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter rows : ");
+        int rows=sc.nextInt();
+        int count=65;
+
+        //Upper Half of pattern
+        for(int i=1;i<=rows;i++)
+        {
+    
+            for(int j=rows-1;j>=i;j--)
+            {
+                System.out.print("*");
+            }
+
+            for(int j=1;j<=i;j++)
+            {
+                if(j==1){
+                System.out.print((char)(count));
+                }
+                else
+                {                
+                    System.out.print("*");
+                }
+            }
+            for(int j=2;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            if(i!=1){
+                System.out.print((char)(count));
+                count++;
+            }
+
+            System.out.println();
+        }
+
+        
+
+
+    }
+
 
 }
