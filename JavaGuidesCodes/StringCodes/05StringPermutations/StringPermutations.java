6d10c2a192d60e88ad45b853aea149496c02481b
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-12 19:37:15 +0530

    Word permutations

---
+++ JavaGuidesCodes/StringCodes/05StringPermutations/StringPermutations.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class StringPermutations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Word : ");
+        String word = sc.next();
+
+        generatePermutations(word,"");
+        System.out.println("All permutations of "+ word +" have been displayed.");
+    }
+
+    private static void generatePermutations(String str,String ans) 
+    {
+       if(str.length()==0)
+       {
+            System.out.println(ans);
+            return;
+       }
+       for(int i=0;i<str.length();i++)
+       {
+            char ch = str.charAt(i);
+            String remaining= str.substring(0, i) + str.substring(i+1);
+            generatePermutations(remaining, ans+ch);
+       }
+    }
+}
