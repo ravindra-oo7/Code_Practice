cc2249d9ad3b1a8fd9e6da19899ec8e3b2fe5d11
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-12 16:19:29 +0530

    Duplicate Character Finder

---
+++ JavaGuidesCodes/StringCodes/03FindDuplicateCharacters/DuplicateCharsFinder.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DuplicateCharsFinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String to Duplicate Characters in String : ");
+        String str = sc.nextLine();
+
+        Map<Character, Integer > strMap = new HashMap<>();
+
+
+        for (char e : str.toCharArray()) {
+            strMap.put(e, strMap.getOrDefault(e,0)+1);            
+        }
+        
+        System.out.println("Duplicate Characters : ");
+        for (Map.Entry<Character,Integer> entry :strMap.entrySet()) {
+            if(entry.getValue()>1)
+            {
+                System.out.print(entry.getKey()+" ");
+            }
+        }
+    }
+}
