c346f20cf88f424e9f91196c4eac7fd950922d7a
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-13 16:29:13 +0530

    New Directory added

---
+++ TuleskoJavaPractice/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int num= 22222;
+        System.out.println(numMin);
+
+        byte btMax = 127;//max
+        System.out.println(btMax);
+        byte btMin = -128;
+        System.out.println(btMin);
+
+
+    }
+}
