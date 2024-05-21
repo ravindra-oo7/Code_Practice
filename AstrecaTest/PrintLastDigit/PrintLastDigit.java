f8b1c7384ea21fe24d06833ff037f54c882e6377
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-21 16:25:06 +0530

    Print Last Digit of Integer- Input number is in  String format

--- AstrecaTest/PrintLastDigit/PrintLastDigit.java
+++ AstrecaTest/PrintLastDigit/PrintLastDigit.java
@@ -2,15 +2,31 @@ package PrintLastDigit;
 import java.util.Scanner;
 
 public class PrintLastDigit {
-    public static void main(String[] args) {
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in); 
+    //     System.out.print("Enter any natural number : ");
+    //     int numb1 = sc.nextInt();
+
+    //     while (numb1>9) {
+    //         numb1=numb1%10;
+    //     }
+    //     System.out.println("Last Digit is "+ numb1);
+
+    // }
+
+      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
         System.out.print("Enter any natural number : ");
-        int numb1 = sc.nextInt();
+        String numb1 = sc.next();
+        Integer temp = Integer.valueOf(numb1);
 
-        while (numb1>9) {
-            numb1=numb1%10;
+        while (temp>9) {
+            temp%=10;
         }
-        System.out.println("Last Digit is "+ numb1);
+        System.out.println("Last Digit is "+ temp);
 
     }
+
+
 }
