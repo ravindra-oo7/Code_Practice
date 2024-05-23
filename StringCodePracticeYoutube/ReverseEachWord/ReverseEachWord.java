8442423dc982a954eeae746e19c2bae5de7ae22d
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-23 20:02:44 +0530

    Reverse Each word of a Sentence Approach-02

--- StringCodePracticeYoutube/ReverseEachWord/ReverseEachWord.java
+++ StringCodePracticeYoutube/ReverseEachWord/ReverseEachWord.java
@@ -8,14 +8,27 @@ public class ReverseEachWord {
         System.out.print("Enter Sentence : ");
         String str = sc.nextLine();
 
-        //Approach-01
         String[] strArr = str.split(" ");
-
+ /*        
+        //Approach-01
         for(int i=0;i<strArr.length;i++)
         {
            String str1 = strArr[i];
             System.out.print(new StringBuilder(str1).reverse()+" ");   
         }
+ */
+        //Approach-02
+        for(int i=0;i<strArr.length;i++)
+        {
+            String str1 = strArr[i];
+            for(int j=str1.length()-1;j>=0;j--)
+            {
+                System.out.print(str1.charAt(j));
+            }
+            System.out.print(" ");
+
+        }
+
 
     }
 }
