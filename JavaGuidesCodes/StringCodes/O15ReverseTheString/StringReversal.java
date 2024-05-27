c7bceb1a02e375899b8fcbb7d9739b030d007675
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-27 15:42:59 +0530

    String Reversal Approach-02 and Approach-03

--- JavaGuidesCodes/StringCodes/O15ReverseTheString/StringReversal.java
+++ JavaGuidesCodes/StringCodes/O15ReverseTheString/StringReversal.java
@@ -9,10 +9,32 @@ public class StringReversal {
         System.out.print("Enter String : ");
         String strInput = sc.next();
 
+/* 
         //Approach-01 Using StringBuilder
         StringBuilder strBD = new StringBuilder(strInput);
         System.out.println(strBD.reverse());
+ */
 
-        
+        //Approach-02 Using a Character Array
+        char [] charArr = strInput.toCharArray();
+        for(int i=charArr.length-1;i>=0;i--)
+        {
+            System.out.print(charArr[i]);
+        }
+
+        //Approach-03 Using Recursion
+        System.out.println(reverseString(strInput));
+
+    }
+
+    private static String reverseString(String str) 
+    {
+       if(str.isEmpty())
+       {
+        return str;
+       }
+       else{
+        return reverseString(str.substring(1))+str.charAt(0);
+       }
     }
 }
