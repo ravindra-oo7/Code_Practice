8cde5912cc3a09e1bc9b815640addf86c3e11e0e
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-29 15:34:35 +0530

    Each Word reversal Approach-02

--- JavaGuidesCodes/StringCodes/O16ReverseEachWord/ReverseEachWord.java
+++ JavaGuidesCodes/StringCodes/O16ReverseEachWord/ReverseEachWord.java
@@ -11,11 +11,24 @@ public class ReverseEachWord {
 
         String [] strArr = str.trim().split("\\s+");
 
-        //Approach-01
+/*         
+    //Approach-01 StringBuilder
         for (String string : strArr) 
         {
             StringBuilder strBD = new StringBuilder(string);
             System.out.print(strBD.reverse()+" ");
+        }
+
+*/        
+
+    //Approach-02 Using str.charAt() method
+        for (String word : strArr) 
+        {
+            for(int i=word.length()-1;i>=0;i--)
+            {
+                System.out.print(word.charAt(i));;
+            }
+            System.out.print(" ");
         }
 
         
