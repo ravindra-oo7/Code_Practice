9c2984cf5784a0a1005669aabeaaf74f04710625
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-30 10:24:29 +0530

    String Whitespace Remove Approach-01

---
+++ JavaGuidesCodes/StringCodes/O21RemoveAllWhitespaces/RemoveAllWhiteSpaces.java
@@ -0,0 +1,27 @@
+package StringCodes.O21RemoveAllWhitespaces;
+
+import java.util.Scanner;
+
+public class RemoveAllWhiteSpaces {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String : ");
+        String str = sc.nextLine();
+
+/*     
+    //Approach-01
+        StringBuilder strBD = new StringBuilder();
+        String [] strArr = str.trim().split("\\s+");   
+        
+        for (String string : strArr) 
+        {
+            strBD.append(string);
+        }
+        System.err.println(strBD);
+*/
+    //Approach-o2
+            
+
+    }
+}
