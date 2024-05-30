84fbe50f99394c8ea96ce8d2abef4455d2c9f2a5
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-05-30 15:30:53 +0530

    Print ABCD

---
+++ JavaGuidesCodes/StringCodes/O26PrintAlphabetsFromAtoZ/PrintABCD.java
@@ -0,0 +1,22 @@
+package StringCodes.O26PrintAlphabetsFromAtoZ;
+
+public class PrintABCD {
+    public static void main(String[] args) 
+    {
+ /*        
+        //Approach-01
+        for(char ch ='A';ch<='Z';ch++)
+        {
+            System.out.print(ch+" ");
+        }
+*/
+        //Approach-02
+        for(int i = 65;i<=90;i++)
+        {
+            System.out.print((char)i+" ");
+        }
+        
+
+
+    }
+}
