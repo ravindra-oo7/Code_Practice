d00f794c418a4289913ad3a013205767b9cdf34f
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-01 19:23:10 +0530

    Prime Numbers Printer  Optimized Way

--- JavaGuidesCodes/NumberCodes/O04FindNumbers/FindPrimeNumb.java
+++ JavaGuidesCodes/NumberCodes/O04FindNumbers/FindPrimeNumb.java
@@ -8,9 +8,11 @@ public class FindPrimeNumb {
         Scanner sc = new Scanner(System.in); 
         System.out.print("Enter Upper Limit : ");
         int upperLimit = sc.nextInt();
-        System.out.print("Prime Numbers upto "+upperLimit+" : 2");
-        
-        for(int i=3;i<=upperLimit;i++)
+        System.out.print("Prime Numbers upto "+upperLimit+" : 2, 3");
+
+/*         
+        //Approach-01
+        for(int i=4;i<=upperLimit;i++)
         {
             boolean isPrime = true;
             for(int j=2;j<i;j++)
@@ -27,6 +29,46 @@ public class FindPrimeNumb {
                 System.out.print(" ,"+i);
             }
         }
+ */
+
+/* 
+        //Optimized way
+        for(int i=4;i<=upperLimit;i++)
+        {
+            boolean isPrime = true;
+            for(int j=2;j<=i/2;j++)
+            {
+                if(i%j==0)
+                {
+                    isPrime=false;
+                    break;
+                }
+            }
+
+            if(isPrime)
+            {
+                System.out.print(" ,"+i);
+            }
+        }
+*/
+         // Most Optimized way
+         for(int i=4;i<=upperLimit;i++)
+         {
+             boolean isPrime = true;
+             for(int j=2;j<=Math.sqrt(i);j++)
+             {
+                 if(i%j==0)
+                 {
+                     isPrime=false;
+                     break;
+                 }
+             }
+ 
+             if(isPrime)
+             {
+                 System.out.print(" ,"+i);
+             }
+         }
 
 
     }
