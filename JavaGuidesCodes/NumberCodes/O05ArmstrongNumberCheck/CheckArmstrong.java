f1148bb6e0c7efbfee8f8a648e7977b6085760a3
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-02 15:50:50 +0530

    Check if number is Armstrong

---
+++ JavaGuidesCodes/NumberCodes/O05ArmstrongNumberCheck/CheckArmstrong.java
@@ -0,0 +1,35 @@
+package NumberCodes.O05ArmstrongNumberCheck;
+
+import java.util.Scanner;
+
+public class CheckArmstrong {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter Number : ");
+        int numb = sc.nextInt();
+        String str = String.valueOf(numb);
+        
+/*         
+        //Approach-01
+        int sum = 0;
+        int length = str.length();
+        for(int i=0;i<length;i++)
+        {
+            int temp = Character.getNumericValue(str.charAt(i));
+            sum = (int)(sum+(Math.pow(temp, length)));
+        }
+        System.out.println("numb = "+numb+" | sum = "+sum);
+        if(sum==numb)
+        {
+            System.out.println(numb+" is Armstrong Number. ");
+        }
+        else
+        {
+            System.out.println(numb+" is not Armstrong number !!");
+        }
+*/
+        int sum = 0;
+
+    }
+}
