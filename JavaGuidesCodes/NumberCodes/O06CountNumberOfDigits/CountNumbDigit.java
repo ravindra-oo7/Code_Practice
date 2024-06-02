345be462b7234bfc69cfe8c63cd1300fad4e323e
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-02 16:08:05 +0530

    Number Of Digits in Entered number Approach-02

--- JavaGuidesCodes/NumberCodes/O06CountNumberOfDigits/CountNumbDigit.java
+++ JavaGuidesCodes/NumberCodes/O06CountNumberOfDigits/CountNumbDigit.java
@@ -9,10 +9,19 @@ public class CountNumbDigit {
         System.out.print("Enter Number : ");
         int numb = sc.nextInt();
 
-
+/* 
         //Approach-01
         String str = String.valueOf(numb);
         System.out.println("Number of digits in entered number is "+ str.length());
-        
+*/        
+        //Approach-02
+        int count = 0;
+        while(numb>0)
+        {
+            count++;
+            numb = numb/10;
+        }
+        System.out.println("Length of given number : "+count);
+
     }
 }
