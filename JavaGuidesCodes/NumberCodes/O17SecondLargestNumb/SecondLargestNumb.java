7db566cb87fd3face5018443842ab9a2805db181
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-03 17:05:01 +0530

    Second Largest Number Approach-02

--- JavaGuidesCodes/NumberCodes/O17SecondLargestNumb/SecondLargestNumb.java
+++ JavaGuidesCodes/NumberCodes/O17SecondLargestNumb/SecondLargestNumb.java
@@ -17,13 +17,33 @@ public class SecondLargestNumb {
             arr[i]=sc.nextInt();
         }
         System.out.println("Array : "+Arrays.toString(arr));
-
+/* 
     //Approach-01
         //Sort array
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
         System.out.println("2nd largest element : "+arr[arr.length-2]);
+*/
 
-        
+    //Approach-02
+        int largest = 0;
+        int secondLar = 0;
+
+        for(int numb:arr)
+        {
+            if(numb>=largest)
+            {
+                secondLar=largest;
+                largest=numb;
+            }
+            else if(numb>=secondLar && numb<largest)
+            {
+                secondLar=numb;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Second largest number : "+secondLar);
+
     }
 }
