47ce334fe0a57fea560e4c6436df168fb43ba253
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-03 11:18:09 +0530

    Strong Number Cheacker

---
+++ JavaGuidesCodes/NumberCodes/O13StrongNumber/DigitalNumber/StrongNumber.java
@@ -0,0 +1,36 @@
+package NumberCodes.O13StrongNumber.DigitalNumber;
+
+import java.util.Scanner;
+
+public class StrongNumber {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter Number : ");
+        int numbInput = sc.nextInt();
+
+        int numb = numbInput;
+        int sum = 0;
+        while(numb!=0)
+        {
+            int temp = numb%10;
+            numb=numb/10;
+            int fact=1;
+            for(int i=1;i<=temp;i++)
+            {
+                fact = fact*i;
+            }
+            sum=sum+fact;
+        }
+
+        System.out.println("Sum is :"+sum);
+        if(sum==numbInput)
+        {
+            System.out.println(numbInput+ " is Strong Number.");
+        }
+        else
+        {
+            System.out.println(numbInput+" Is Not Strong Number !!!");
+        }
+    }
+}
