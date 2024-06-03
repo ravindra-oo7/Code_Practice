ae9135d1bc6911d82932432d6521a37c74985c1d
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-03 17:37:15 +0530

    Find if Given number is perfect Square Approach-01

---
+++ JavaGuidesCodes/NumberCodes/O26CheckIfPerfectSquare/IsPerfectSquare.java
@@ -0,0 +1,23 @@
+package NumberCodes.O26CheckIfPerfectSquare;
+
+import java.util.Scanner;
+
+public class IsPerfectSquare {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int numb = Math.abs(sc.nextInt());
+
+        //Approach-01
+        double sqrt = Math.sqrt(numb);
+        if(sqrt == (int)sqrt)
+        {
+            System.out.println(numb+" is Perfect square ");
+        }
+        else
+        {
+            System.out.println(numb+" is NOT Perfect square ");
+        }
+    }
+}
