c5a0401f37101cf61bdd52f1664a8af7ff7f2865
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-03 13:43:14 +0530

    Swapping Two Numbers Approach-02

--- JavaGuidesCodes/NumberCodes/O16SwapTwoNumbs/SwapNumbs.java
+++ JavaGuidesCodes/NumberCodes/O16SwapTwoNumbs/SwapNumbs.java
@@ -10,14 +10,22 @@ public class SwapNumbs {
         int numb1 = sc.nextInt();
         System.out.print("Enter  second Number : ");
         int numb2 = sc.nextInt();
-
+/* 
         //Approach-01
         int temp = numb1;
         numb1=numb2;
         numb2=temp;
         System.out.println("After Swapping ");
         System.out.println("Numb1 : "+numb1+" | Numb2 : "+numb2);
+ */
 
+
+        //Approach-02 without temp variable
+        numb1 = numb1+numb2;
+        numb2=numb1-numb2;
+        numb1=numb1-numb2;
+        System.out.println("After Swapping ");
+        System.out.println("Numb1 : "+numb1+" | Numb2 : "+numb2);
 
     }
 }
