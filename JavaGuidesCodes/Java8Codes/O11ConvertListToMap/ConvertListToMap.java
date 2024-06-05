61997899ae92f8a7ec72aca2e078f57e0aa50658
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-05 15:43:38 +0530

    Convert List into Map Approach-02

--- JavaGuidesCodes/Java8Codes/O11ConvertListToMap/ConvertListToMap.java
+++ JavaGuidesCodes/Java8Codes/O11ConvertListToMap/ConvertListToMap.java
@@ -1,15 +1,18 @@
 package Java8Codes.O11ConvertListToMap;
 
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ConvertListToMap {
     public static void main(String[] args) 
     {
         List<String> items = List.of("apple", "banana", "cherry");
 
-
+/* 
         //Approach-01
         Map<String,Integer> strMap = new LinkedHashMap<>();
 
@@ -22,8 +25,14 @@ public class ConvertListToMap {
         {
             System.out.println("Key : "+entry.getKey()+" | Value : "+entry.getValue());
         }
+*/
 
+        //Approach-02
+       Map<String,Integer> strMap =  items.stream()
+                                            .collect(Collectors.toMap(item->item, item->item.length()));
 
-        
+        System.out.println("List to Map : ");
+        strMap.forEach((key,value)->System.out.println("Key : "+key+ " | Value : "+value));
+
     }
 }
