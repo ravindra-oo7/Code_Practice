bc36dd76075ba1dbb6cebe4be118eca82125b9ee
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-05 15:22:14 +0530

    Sort String Words Approach-02

--- JavaGuidesCodes/Java8Codes/O10SortListOfString/SortListOfString.java
+++ JavaGuidesCodes/Java8Codes/O10SortListOfString/SortListOfString.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortListOfString {
     public static void main(String[] args) 
     {
         List<String> strList = Arrays.asList("Banana", "Apple", "Orange", "Mango", "Grape");
-
+        System.out.println("\nList : "+strList);
+/*         
         //Approach-01
         strList.sort(new Comparator<String>() {
             @Override
@@ -19,5 +21,13 @@ public class SortListOfString {
             }
         });
         System.out.println("Sorted list : "+strList);
+*/
+        //Approach-02
+        List<String> sortedStrings = strList.stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println("\nSorted list : "+sortedStrings);
+
     }
 }
