aec7033aa5f29654597fdcbe56104d0b0f29275d
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-06 11:05:23 +0530

    Character Counter Approach-02 and Approach-03

--- JavaGuidesCodes/Java8Codes/O12CountCharacters/CountCharactersInString.java
+++ JavaGuidesCodes/Java8Codes/O12CountCharacters/CountCharactersInString.java
@@ -3,6 +3,8 @@ package Java8Codes.O12CountCharacters;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CountCharactersInString {
     public static void main(String[] args) 
@@ -11,6 +13,8 @@ public class CountCharactersInString {
         System.out.print("Enter String : ");
         String str = sc.next().toLowerCase();
 
+/*         
+        //Approach-01
         Map<Character,Integer> charMap = new LinkedHashMap<>();
         for(int i=0;i<str.length();i++)
         {
@@ -22,6 +26,27 @@ public class CountCharactersInString {
         {
             System.out.println("Character : "+entry.getKey()+" | Count : "+entry.getValue());
         }
+*/
 
+/* 
+        //Approach-02
+        str.chars()
+            .mapToObj(c->(char)c)
+            .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+            .entrySet().stream()
+            .forEach(entry->System.out.println("Character : "+entry.getKey()+", Count : "+entry.getValue()));
+
+*/
+
+/* 
+        //Approach-03 Print only duplicate characters
+        str.chars()
+        .mapToObj(c->(char)c)
+        .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+        .entrySet().stream().filter(entry->entry.getValue()>1)
+        .forEach(entry->System.out.println("Character : "+entry.getKey()+", Count : "+entry.getValue()));
+ */
+
+ 
     }
 }
