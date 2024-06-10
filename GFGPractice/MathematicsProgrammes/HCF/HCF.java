3f5a369db07750e2ea718e63eb161d6251c4f89d
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-10 17:34:24 +0530

    HCF Approach-01

---
+++ GFGPractice/MathematicsProgrammes/HCF/HCF.java
@@ -0,0 +1,28 @@
+package MathematicsProgrammes.HCF;
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int numb1 = sc.nextInt();  
+        System.out.print("Enter Second Number : ");
+        int numb2 = sc.nextInt();   
+
+
+        //Approach-01
+        int lowestNumb = Math.min(numb1, numb2);
+        int hcf = 1;
+        for(int i=2;i<=lowestNumb;i++)
+        {
+            if(numb1%i == 0 && numb2%i == 0)
+            {
+                hcf=i;
+            }
+        }
+        System.err.println("HCF : "+hcf);
+
+        
+    }
+}
