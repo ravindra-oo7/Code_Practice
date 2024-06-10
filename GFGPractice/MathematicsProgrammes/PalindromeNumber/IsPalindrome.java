518fbc05cea2a3b52777bb582e364de6f959b686
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-10 14:09:12 +0530

    Is Palindrome Approach-02

--- GFGPractice/MathematicsProgrammes/PalindromeNumber/IsPalindrome.java
+++ GFGPractice/MathematicsProgrammes/PalindromeNumber/IsPalindrome.java
@@ -8,8 +8,8 @@ public class IsPalindrome {
         System.out.print("Enter Number : ");
         int numb = sc.nextInt();
 
+/*         
         //Approach-01
-
         StringBuilder strBD = new StringBuilder(Integer.toString(numb));
 
         if(Integer.toString(numb).equals(strBD.toString()))
@@ -19,6 +19,25 @@ public class IsPalindrome {
         else
         {
             System.out.println("Not Palindrome!!");
+        }
+*/
+
+        //Approach-02
+        int temp = numb;
+        int reversed = 0;
+        while(temp!=0)
+        {
+            reversed = reversed*10+temp%10;
+            temp/=10;
+        }
+
+        if(reversed==numb)
+        {
+            System.out.println(numb +" is Palindrome.");
+        }
+        else
+        {
+            System.out.println(numb+" is Not Palindrome!!");
         }
 
 
