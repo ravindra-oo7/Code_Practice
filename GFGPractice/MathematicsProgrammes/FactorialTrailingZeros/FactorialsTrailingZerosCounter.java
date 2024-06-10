0d2d68cb7ad12a5247cd68c1c17b53a1327be29e
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-10 17:00:00 +0530

    Factorials Trailing Zeros Counter

---
+++ GFGPractice/MathematicsProgrammes/FactorialTrailingZeros/FactorialsTrailingZerosCounter.java
@@ -0,0 +1,35 @@
+package MathematicsProgrammes.FactorialTrailingZeros;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class FactorialsTrailingZerosCounter {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        long numb = sc.nextInt();
+
+        //Approach-01
+        //Calculate factorial using BigInteger
+        BigInteger fact =  BigInteger.ONE;
+        
+        for(int i=2;i<=numb;i++)
+        {
+            fact = fact.multiply(new BigInteger(Integer.toString(i)));
+        }
+        System.out.println("Factorial of "+numb+" : "+fact);
+
+        // Count trailing zeros
+        int counter = 0;
+        BigInteger ten = BigInteger.TEN;
+        while (fact.mod(ten).equals(BigInteger.ZERO))
+        {
+                fact = fact.divide(ten);
+                counter++;
+        }
+
+        System.out.println("Trailing Zeros : "+counter);
+
+    }
+}
