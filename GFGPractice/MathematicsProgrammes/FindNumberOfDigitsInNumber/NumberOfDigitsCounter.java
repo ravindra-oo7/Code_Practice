1ead93b81b5a7ea2d7d5f6d158616f4bc64358a0
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-10 12:44:25 +0530

    Counting number of digits in Input Number Approach-02 and 03

--- GFGPractice/MathematicsProgrammes/FindNumberOfDigitsInNumber/NumberOfDigitsCounter.java
+++ GFGPractice/MathematicsProgrammes/FindNumberOfDigitsInNumber/NumberOfDigitsCounter.java
@@ -9,6 +9,7 @@ public class NumberOfDigitsCounter {
         System.out.print("Enter Number : ");
         int numb = sc.nextInt();
 
+/* 
         //Approach-01
         int count=0;
         while(numb!=0)
@@ -16,9 +17,26 @@ public class NumberOfDigitsCounter {
             numb=numb/10;
             count++;
         }
-
         System.out.print("Number of Digits in Input Number :"+ count);
+*/
+
+/* 
+        //Approach-02
+        System.out.println("Number of Digits in "+numb+" :"+countDigit(numb));
+*/
+
+        //Approach-03
+        System.out.println("Digits count : "+(int)(Math.floor(Math.log10(numb)+1)));
+
+    }
 
 
+
+    static int countDigit(int n)
+    {
+        if(n==0)
+            return 0;
+        
+        return 1 +countDigit(n/10);
     }
 }
