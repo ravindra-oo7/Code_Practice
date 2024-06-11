7854a5dfe7f9aaec1f5d7e8f62921cf461426dac
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-11 20:14:58 +0530

    Find Factors Sorted output Approach-03

--- GFGPractice/MathematicsProgrammes/FindAllFactors/FindAllFactors.java
+++ GFGPractice/MathematicsProgrammes/FindAllFactors/FindAllFactors.java
@@ -22,7 +22,7 @@ public class FindAllFactors {
 
 */        
         
-
+/* 
         // Approach-02 Optimised
         System.out.print("All Factors : ");
         for(int i=1;i*i<=numb;i++)
@@ -36,6 +36,26 @@ public class FindAllFactors {
                 }
             }
         }
+*/
+
+        //Approach-03 same as above but output is Sorted
+        System.out.print("All Factors : ");
+        int i;
+        for(i=1;i*i<numb;i++)
+        {
+            if(numb%i==0)
+            {
+                System.out.print(i+", ");
+            }
+        }
+        for(;i>=1;i--)
+        {
+            if(numb%i==0)
+            {
+                System.out.print(numb/i+", ");
+            }
+        }
+
         
     }
 }
