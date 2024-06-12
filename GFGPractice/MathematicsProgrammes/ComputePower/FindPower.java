d9ab1f7eec853ece59a1c7e8dcc8fef08c12468f
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-12 17:01:35 +0530

    Finding numbers Power Approach-01

---
+++ GFGPractice/MathematicsProgrammes/ComputePower/FindPower.java
@@ -0,0 +1,23 @@
+package MathematicsProgrammes.ComputePower;
+import java.util.Scanner;
+
+public class FindPower {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Numb1 : ");
+        int base = sc.nextInt();
+        System.out.print("Enter Numb2 : ");
+        int pow = sc.nextInt();
+
+
+        // Approch-01
+        int result =1;
+        for(int i=0;i<pow;i++)
+        {
+            result = result * base;
+        }
+        System.out.println(base+" to the Power "+pow+" : "+result);
+
+    }
+}
