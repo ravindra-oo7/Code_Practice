ebc0e2e41785115c14f7600d05611e203303f61c
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-14 09:18:34 +0530

    Bitwise OR operator

--- GFGPractice/BitMagic/BitwiseOperators/ORopt.java
+++ GFGPractice/BitMagic/BitwiseOperators/ORopt.java
@@ -1,20 +1,20 @@
 package BitMagic.BitwiseOperators;
 
-public class AndOpt {
+public class ORopt {
     public static void main(String[] args) 
     {
         int x=3,y=6;
-        System.out.println(x&y);
-        
+        System.out.println(x|y);
     }
 }
+
 /* for 8 bit  Architecture
 
     x=3 : 0000 0011
     y=6 : 0000 0110
     ----------------
-    x&y : 0000 0010 => 2
+    x&y : 0000 0111 => 4+2+1 =7
 
-    ANS = 2 
+    ANS = 7 
 
 */
