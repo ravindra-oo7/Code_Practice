250907bb7255a6d0d2ad09689d3cbac8de4f929d
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-14 09:11:49 +0530

    Bitwise AND operator

---
+++ GFGPractice/BitMagic/BitwiseOperators/AndOpt.java
@@ -0,0 +1,19 @@
+package BitMagic.BitwiseOperators;
+
+public class AndOpt {
+    public static void main(String[] args) 
+    {
+        int x=3,y=6;
+        System.out.println(x&y);
+        
+    }
+}
+/* 
+    x=3 : 0000 0011
+    y=6 : 0000 0110
+    ----------------
+    x&y : 0000 0010 => 6
+
+    ANS = 6 
+
+*/
