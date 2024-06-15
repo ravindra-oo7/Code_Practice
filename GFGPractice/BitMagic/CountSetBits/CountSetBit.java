648d32204767719065d102b0f0ec614855f7ebbb
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-15 15:23:38 +0530

    Count Set Bit Approach-01

---
+++ GFGPractice/BitMagic/CountSetBits/CountSetBit.java
@@ -0,0 +1,27 @@
+package BitMagic.CountSetBits;
+
+import java.util.Scanner;
+
+public class CountSetBit 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);       
+        System.out.print("Enter Number : ");
+        int numb = sc.nextInt();
+        
+        // Approach-01
+        int counter=0;
+        while(numb>0)        
+        {
+            if ((numb & 1)==1) 
+            {
+                counter++;
+            }
+            
+            numb = numb>>1;
+        }
+        
+        System.out.println("Count : "+counter);
+    }
+}
