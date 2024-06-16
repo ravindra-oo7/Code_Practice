f644e75a5bd08072871c7a1e88af4b16ced688c0
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-16 11:42:02 +0530

    Find the number whose occurance is Odd Approach-01

---
+++ GFGPractice/BitMagic/FindOddOcurringNumbs/FindOddOccurringNumbs.java
@@ -0,0 +1,50 @@
+package BitMagic.FindOddOcurringNumbs;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class FindOddOccurringNumbs {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);       
+        System.out.print("Enter Array Length : ");
+        int numb = sc.nextInt();
+
+        int[] intArr = new int[numb];
+        System.out.println("Enter elements in Array : ");
+        for(int i=0;i<intArr.length;i++)
+        {
+            System.out.print("Element "+(i+1)+" :");
+            intArr[i] = sc.nextInt();
+        }
+        System.err.println("Input Array : "+Arrays.toString(intArr));
+
+
+        //Approach-01
+        System.out.print("Odd Occurring Numbers : ");
+        Set<Integer> intSet = new LinkedHashSet<>();
+
+        for(int i=0;i<intArr.length;i++)
+        {
+            int count=0; 
+            if(intSet.add(intArr[i]))
+            {
+                for(int j=i;j<intArr.length;j++)
+                {
+                    if(intArr[i]==intArr[j])
+                        count++;
+                }     
+            }
+
+            if(count%2!=0)
+            {
+                System.out.print(intArr[i]+", ");
+            }
+        }
+
+
+
+    }
+}
