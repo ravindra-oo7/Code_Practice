79061a7d566781215551d56fae0cd6194b211dce
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-17 15:44:27 +0530

    Print N numbers in Decending Order Approach-02

--- GFGPractice/Recursion/PrintNNumbs/PrintNumbersNto1.java
+++ GFGPractice/Recursion/PrintNNumbs/PrintNumbersNto1.java
@@ -8,13 +8,29 @@ public class PrintNumbersNto1 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Numb : ");
         int numb = sc.nextInt();
-        
+
+/*         
         //Approach-01
         for(int i=numb;i>0;i--)
         {
             System.out.print(" "+i);
         }
+*/
 
+        // Approach-02 Recursive
+        printNumb(numb);
+
+
+    }
+    static void printNumb(int n)
+    {
+        if(n==0)
+        {
+            return;
+           // System.exit(0);
+        }
+        System.out.print(" "+n);
+        printNumb(n-1);
     }
     
 }
