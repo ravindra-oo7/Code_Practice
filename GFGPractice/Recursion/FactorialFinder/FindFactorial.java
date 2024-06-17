8107392b1a1535862c6ad3d8673aa87571349bf2
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-17 17:16:22 +0530

    Factorial Finder Approach-02

--- GFGPractice/Recursion/FactorialFinder/FindFactorial.java
+++ GFGPractice/Recursion/FactorialFinder/FindFactorial.java
@@ -9,6 +9,7 @@ public class FindFactorial {
         System.out.print("Enter Numb : ");
         int numb = sc.nextInt();
 
+/*         
         // Approach-01
         int fact = 1;
         for(int i=1;i<=numb;i++)
@@ -16,6 +17,21 @@ public class FindFactorial {
             fact = fact*i;
         }
         System.out.println("Factorial : "+fact);
+*/
 
+        // Approach-02
+        System.out.println("Factorial : "+factFind(numb));
+
+
+    }
+
+    private static int factFind(int numb) 
+    {
+       int fact=numb;
+       if(numb==0||numb==1)
+       {
+         return 1;
+       }
+        return fact*factFind(numb-1);
     }
 }
