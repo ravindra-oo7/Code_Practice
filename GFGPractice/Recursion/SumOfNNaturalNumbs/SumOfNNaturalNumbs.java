e14a4aa229296a4682951983ab6a0022918b9b2f
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-19 11:59:15 +0530

    Sum Of N Natural Numbers Approach-01

---
+++ GFGPractice/Recursion/SumOfNNaturalNumbs/SumOfNNaturalNumbs.java
@@ -0,0 +1,21 @@
+package Recursion.SumOfNNaturalNumbs;
+
+import java.util.Scanner;
+
+public class SumOfNNaturalNumbs {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int numb = sc.nextInt();
+
+        // Approach-01
+        int sum = 0;
+        for(int i=1;i<=numb;i++)
+        {
+            sum = sum+i;
+        }
+
+        System.out.println("Sum : "+sum);
+    }
+}
