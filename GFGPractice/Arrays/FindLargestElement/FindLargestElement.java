a03bbf18bc8a6a93dbdc5cdba7d6e7a66781517d
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-22 21:37:47 +0530

    Find Largest Numbers Index Approach-01

---
+++ GFGPractice/Arrays/FindLargestElement/FindLargestElement.java
@@ -0,0 +1,25 @@
+package Arrays.FindLargestElement;
+
+import java.util.Scanner;
+
+public class FindLargestElement {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int[] intArr = {10,5,20,8};
+
+        int largest = 0;
+        int pointer=0;
+        for(int i=0;i<intArr.length;i++)
+        {
+            if(largest<intArr[i])
+            {
+                largest = intArr[i];
+                pointer = i;
+            }
+        }
+        
+        System.out.print("Largest Elements Index : "+ pointer);
+
+    }
+}
