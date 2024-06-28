42d3cbfed5384abed8d6ce00eaadabd3119c846e
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-28 16:07:44 +0530

    Find Second Largest Element Approch-03

--- GFGPractice/Arrays/FindSecondLargestElement/FindSecondLargestElement.java
+++ GFGPractice/Arrays/FindSecondLargestElement/FindSecondLargestElement.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class FindSecondLargestElement {
     public static void main(String[] args) 
     {
-        int[] intArr = {10,5,20,8,4,65,2,43,2,0,245};
+        int[] intArr = {10,10,5,5,5,4,2};
         System.out.println("Array : "+Arrays.toString(intArr));
 
 
@@ -16,25 +16,55 @@ public class FindSecondLargestElement {
         int secLargest = 0;
         int pointer=0;
         for(int i=0;i<intArr.length;i++)
-        {
-            if(largest<intArr[i])
+        {  
+            if(largest<=intArr[i])
             {
                 int temp = largest; 
                 largest = intArr[i];
                 secLargest = temp;
-            }   
+            }      
         }
         System.out.println("Second Largest Element : "+secLargest);
         System.out.println("Second Largest Elements Index : "+ pointer);
 */
 
+/* 
         // Approach-02
         Arrays.sort(intArr);
         System.out.println("Array After Sort : "+Arrays.toString(intArr));
 
 
         System.out.println("Second Largest Element : "+intArr[intArr.length-2]);
+*/
 
+        // Approach -03
+        int secLrg = -1, lrg = 0;
+        for(int i=1;i<intArr.length;i++)
+        {
+            if(intArr[i]>intArr[lrg])
+            {
+                secLrg = lrg;
+                lrg=i;
+            }
+            else if(intArr[i]!=intArr[lrg])
+            {
+                if(secLrg == -1 || intArr[i]>intArr[secLrg])
+                {
+                    secLrg = i;
+                }
+            }
+        } 
+
+        if(secLrg==-1)
+        {
+            System.out.println("No Second Largest Element in Array");
+        }
+        else
+        {
+            System.out.println("Second Largest Element : " + intArr[secLrg]);
+            System.out.println("Second Largest Element's Index : " + secLrg);
+            
+        }
 
     }
 }
