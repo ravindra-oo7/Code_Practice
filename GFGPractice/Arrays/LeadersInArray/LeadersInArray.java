25f62033c2472b99fd15dc183602c18df8431104
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-02 18:32:59 +0530

    Leaders in Array Approach-02

--- GFGPractice/Arrays/LeadersInArray/LeadersInArray.java
+++ GFGPractice/Arrays/LeadersInArray/LeadersInArray.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class LeadersInArray {
     public static void main(String[] args) 
     {
-        int[] arr = {7,10,4,3,6,5,2};  
+        int[] arr = {7,10,4,3,10,6,5,2};  
         System.out.println("Array : "+Arrays.toString(arr));
 
+/*         
     // Approach-01
         System.out.print("Leaders in Array : ");
         for (int i=0;i<arr.length;i++)
@@ -15,19 +16,31 @@ public class LeadersInArray {
             boolean isLeader = true;
             for(int j=i+1;j<arr.length;j++)
             {
-                if(arr[i]<arr[j])
+                if(arr[i]<=arr[j])
                 {
                     isLeader = false;
                     break;
-                }
+                }           
             }
             if(isLeader)
             {
                 System.out.print(arr[i]+", ");
             }
         }
+*/
+    // Approach-02
+        int currLed = arr[arr.length-1];
+        System.out.print("Leaders in Array : ");
+        System.out.print(currLed+ "  ");
 
-        
+        for(int i=arr.length-2;i>=0;i--)
+        {
+            if(arr[i]>currLed)
+            {
+                System.out.print(arr[i]+"  ");
+                currLed = arr[i];
+            }
+        }
         
     }
 }
