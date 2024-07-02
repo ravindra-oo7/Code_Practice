e0d90e97d2d9d5fb5bda6d08e7839ab5f781c71f
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-02 12:53:00 +0530

    Move Zeros to the end Approach-02

--- GFGPractice/Arrays/MoveZerosToEnd/MoveZerosToEnd.java
+++ GFGPractice/Arrays/MoveZerosToEnd/MoveZerosToEnd.java
@@ -8,6 +8,7 @@ public class MoveZerosToEnd {
         int[] intArr = {4,2,3,2,0,2,0,5,0}; 
         System.out.println("Array Before : "+Arrays.toString(intArr));
 
+/*         
         // Approach-01
         int count = 0;
         int j = 0;
@@ -26,5 +27,27 @@ public class MoveZerosToEnd {
         }
 
         System.err.println("Array After Process : "+Arrays.toString(newArr));
+*/
+
+        // Approach-02
+        for(int i=0;i<intArr.length;i++)
+        {
+            if(intArr[i]==0)
+            {
+                for(int j=i+1;j<intArr.length;j++ )
+                {
+                    if(intArr[j]!=0)
+                    {
+                        int temp = intArr[i];
+                        intArr[i] = intArr[j];
+                        intArr[j] = temp;
+                    }
+                }
+            }
+        }
+        System.err.println("Array After Process : "+Arrays.toString(intArr));
+
     }
+
+   
 }
