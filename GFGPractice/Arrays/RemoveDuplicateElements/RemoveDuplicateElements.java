99e7b78bd19d49b494424885db53e4b509063407
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-02 11:25:37 +0530

    Remove Duplicate Elements From Array Approach-02

--- GFGPractice/Arrays/RemoveDuplicateElements/RemoveDuplicateElements.java
+++ GFGPractice/Arrays/RemoveDuplicateElements/RemoveDuplicateElements.java
@@ -1,6 +1,8 @@
 package Arrays.RemoveDuplicateElements;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RemoveDuplicateElements {
     public static void main(String[] args) 
@@ -9,6 +11,7 @@ public class RemoveDuplicateElements {
         System.out.println("Orignal Array : "+Arrays.toString(intArr));
         System.out.println("Size : "+intArr.length);
 
+/*         
         // Approach-01
         int [] processedArr = Arrays.stream(intArr)
                 .distinct()
@@ -16,6 +19,19 @@ public class RemoveDuplicateElements {
 
         System.out.println("Proccesed Array : "+Arrays.toString(processedArr));
         System.out.println("Size : "+processedArr.length);
+*/
+
+        // Approach-02
+        Set <Integer> intSet = new HashSet<>();
+        for (int i=0;i<intArr.length;i++)
+        {
+                intSet.add(intArr[i]);
+        }
         
+        Integer[] newArr = intSet.toArray(new Integer[0]);
+
+        System.out.println("Processed Array : " + Arrays.toString(newArr));
+        System.out.println("Size : " + newArr.length);
+
     }
 }
