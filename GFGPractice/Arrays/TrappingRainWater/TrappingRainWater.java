0f5f9a7b2c4532d797b92e45d692ec5a8e7926c2
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-03 20:18:43 +0530

    Trapping Rain water Approach-02 improved

--- GFGPractice/Arrays/TrappingRainWater/TrappingRainWater.java
+++ GFGPractice/Arrays/TrappingRainWater/TrappingRainWater.java
@@ -5,9 +5,11 @@ public class TrappingRainWater {
     {
         //int[] rainArr = {3,0,1,2,5}; //OP = 6 
         //int[] rainArr = {3,5,2,3}; //OP = 1
-        int[] rainArr = {3,0,1,2,5,0,1}; // OP = 7
+        //int[] rainArr = {3,0,1,2,5,0,1}; // OP = 7
+        int[] rainArr = {0,5,2,3,6}; //OP = 5
 
-        //Approach-01
+/* 
+    //Approach-01
         int water = 0;
         for(int i=1;i<rainArr.length;i++)
         {
@@ -26,6 +28,32 @@ public class TrappingRainWater {
             water = water +(Math.min(lMax,rMax)-rainArr[i]);
         }    
         
+*/
+    // Approach-02
+        int water = 0;
+        int len = rainArr.length;
+        int[] lMax = new int[len];
+        int[] rMax = new int[len];
+
+        // setting lMax 
+        lMax[0] = rainArr[0];
+        for(int i=1;i<len;i++)
+        {
+            lMax[i] = Math.max(lMax[i-1], rainArr[i]);
+        }
+
+        // Setting rMax
+        rMax[len-1] = rainArr[len-1];
+        for(int i=len-2;i>=0;i--)
+        {
+            rMax[i] = Math.max(rMax[i+1], rainArr[i]);
+        }
+        
+        for(int i=1;i<len-1;i++)
+        {
+            water = water + (Math.min(lMax[i],rMax[i])-rainArr[i]);
+        }
+
         System.out.println("Water Trapped : "+water);
     }
 }
