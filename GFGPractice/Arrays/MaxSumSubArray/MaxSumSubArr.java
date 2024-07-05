9cabe09c1b360cef17743620b9f21164572f87ec
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-05 13:15:36 +0530

    Max Sum Of SubArray  Approach-02

--- GFGPractice/Arrays/MaxSumSubArray/MaxSumSubArr.java
+++ GFGPractice/Arrays/MaxSumSubArray/MaxSumSubArr.java
@@ -5,6 +5,7 @@ public class MaxSumSubArr {
     {
         int[] arr = {1,-2,3,-1,2};
         
+/*         
         // Approach-01
         int max = arr[0];
         int len = arr.length;
@@ -18,6 +19,18 @@ public class MaxSumSubArr {
                 max = Math.max(max, curr);
             }
         }
-        System.out.println("max Sum = "+max);
+        System.out.println("Max Sum = "+max);
+*/
+
+        // Approach-02
+        int max = arr[0];
+        int maxEnd = arr[0];
+
+        for(int i=1;i<arr.length;i++)
+        {
+            maxEnd = Math.max(maxEnd+arr[i], arr[i]);
+            max = Math.max(maxEnd, max);
+        }
+        System.out.println("Max Sum = "+max);
     }
 }
