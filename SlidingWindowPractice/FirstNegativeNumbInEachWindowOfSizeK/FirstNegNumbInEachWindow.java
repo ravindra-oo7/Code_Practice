6895ca8f3f40f830d24812060d103de50717246e
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-13 11:57:41 +0530

    First Negative Number In Each Window Approach-02

--- SlidingWindowPractice/FirstNegativeNumbInEachWindowOfSizeK/FirstNegNumbInEachWindow.java
+++ SlidingWindowPractice/FirstNegativeNumbInEachWindowOfSizeK/FirstNegNumbInEachWindow.java
@@ -1,6 +1,10 @@
 package FirstNegativeNumbInEachWindowOfSizeK;
 
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class FirstNegNumbInEachWindow {
@@ -15,6 +19,7 @@ public class FirstNegNumbInEachWindow {
         System.out.print("Enter SubArray Size : ");
         int k = sc.nextInt();
 
+/*         
         // Approach-01
 
         for(int i=0;i<=len-k;i++) //len=8 k=3 =>5
@@ -32,6 +37,52 @@ public class FirstNegNumbInEachWindow {
                 }
             }
         }
+*/
+
+        // Approach-02
+
+        List<Integer> window = new LinkedList<>();         
+
+        for(int j=0;j<=len;j++)
+        {
+            if(window.size()<k)
+            {
+                window.add(arr[j]);
+            }
+            else
+            {
+                int i = 0;
+                boolean isPresent = false;
+
+                while (i<k) 
+                {
+                    if(window.get(i)<0)
+                    {
+                        isPresent=true;
+                        System.out.print(window.get(i)+" ");
+                        window.remove(0);
+                        break;
+                    }
+                    else
+                    {
+                        i++;
+                    }    
+                }
+
+                if(!isPresent)
+                {
+                    System.out.print(0+" ");
+                    window.remove(0);
+                }
+
+                if(j<len && window.size()<k)
+                {
+                    window.add(arr[j]);
+                }
+            }
+        }
+
+        
 
 
     }
