60f676c648a8cf0a1306d61b847178ac870f2bd4
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-24 22:28:16 +0530

    Find If Array has Equilibrium Point

--- GFGPractice/Arrays/PrefixSum/FindIfHasEquilibriumPoint/FindIfHasEquiPoint.java
+++ GFGPractice/Arrays/PrefixSum/FindIfHasEquilibriumPoint/FindIfHasEquiPoint.java
@@ -6,10 +6,10 @@ public class FindIfHasEquiPoint {
         int[] arr = {3,4,8,-9,20,6}; // Ans = Yes    20
         //int[] arr = {4,2,-2}; // Yes 4
         //int[] arr = {4,2,2}; // NO
-
-
-        // Approach-01
         boolean hasEquiPoint= false;
+
+/* 
+        // Approach-01
         for(int i = 0;i<arr.length;i++)
         {
             int leftSum=0;
@@ -32,6 +32,29 @@ public class FindIfHasEquiPoint {
                 hasEquiPoint=true;
                 break;
             }
+        }
+
+*/      
+
+        //Approach-02 Prefix Sum Method
+
+        int sum = 0;
+        for(int i=0; i<arr.length;i++)
+        {
+            sum=sum+arr[i];
+        }
+
+        int leftSum = 0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(leftSum==sum-arr[i])
+            {
+                hasEquiPoint=true;
+                System.out.println("Equilibrium Point : "+ arr[i]);
+                break;
+            }
+            leftSum = leftSum + arr[i];
+            sum=sum-arr[i];
         }
 
         if (hasEquiPoint) 
