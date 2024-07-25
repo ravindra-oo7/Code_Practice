666b8bcf3dcafcea15a6f46fd1ec151dd1a4e3e2
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-25 18:01:56 +0530

    Searching Element in Array Approach-02 Binary search

--- GFGPractice/Searching/BinarySearch/SearchElementInArrayReturnIndex.java
+++ GFGPractice/Searching/BinarySearch/SearchElementInArrayReturnIndex.java
@@ -6,8 +6,8 @@ public class SearchElementInArrayReturnIndex
 {
     public static void main(String[] args) 
     {
-        //int[] arr = {1,2,3,4,5,6}; //find k=2
-        int[] arr = {5,15,25}; //k=25
+        int[] arr = {1,2,3,4,5,6}; //find k=2
+        //int[] arr = {5,15,25}; //k=25
         //int[] arr = {5,10,15,25,35}; //k=20
         //int[] arr = {10,10}; //k=10
         
@@ -19,7 +19,7 @@ public class SearchElementInArrayReturnIndex
         int len = arr.length;
         int index = -1;
 
-
+/* 
         // Approach-01
         for(int i=0;i<len;i++)
         {
@@ -34,5 +34,32 @@ public class SearchElementInArrayReturnIndex
             System.out.println(k+" Present at Index : "+index);
         else
             System.out.println(k+" Not Present in Array !!!");
+*/
+        // Approach-02 Binary search
+        int start = 0;
+        int end = len-1;
+
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+
+            if(arr[mid]==k)
+            {
+                System.out.println("Element Found at index : "+mid);
+                System.exit(0);
+            }
+            else if(arr[mid]>k)
+            {
+                end = mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        System.out.println("Element Not Found !!!");
+
+
+
     }
 }
