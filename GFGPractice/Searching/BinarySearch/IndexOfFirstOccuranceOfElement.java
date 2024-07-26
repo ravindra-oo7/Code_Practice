f4cb5fb276a8c135b47f16cecf22e989d3105d02
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-26 11:35:21 +0530

    Return first occurance index in Array Approach-02 Binary search

--- GFGPractice/Searching/BinarySearch/IndexOfFirstOccuranceOfElement.java
+++ GFGPractice/Searching/BinarySearch/IndexOfFirstOccuranceOfElement.java
@@ -35,17 +35,23 @@ public class IndexOfFirstOccuranceOfElement
         if(start>end)
             return -1;
 
-       int mid = (start+end)/2;
-       if(arr[mid]==k)
+       int mid = (start+end)/2;  
+
+       if(arr[mid]>k)
        {
-           return mid;
+            return search(arr, k, start, mid-1);
        }
-       else if(arr[mid]>k)
+       else if(arr[mid]<k)
        {
-           search(arr, k, start, mid-1);
+            return search(arr, k, mid+1, end);
        }
        else
-            search(arr, k, mid+1, end);
-    //return -1;
+       {
+            if(mid==0 || arr[mid-1]!=arr[mid])
+                return mid;
+            else
+                return search(arr, k, start, mid-1) ;   
+       }
+            
     }
 }
