e7c49b5c15ac23ad2d6802da43bf1b70adc01f05
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-27 11:12:37 +0530

    Index of Last Occurance Approach-01 resursive Binary Search Approach

--- GFGPractice/Searching/BinarySearch/IndexOfLastOccurance.java
+++ GFGPractice/Searching/BinarySearch/IndexOfLastOccurance.java
@@ -4,12 +4,16 @@ public class IndexOfLastOccurance
 {
     public static void main(String[] args) 
     {
-        int[] arr = {10,15,20,20,40,40};
-       // int k = 20; // ans = 3
-        int k = 2; // ans = -1
+        int[] arr = {10,10,10,20,20,40,40};
+        int len = arr.length;
 
+       // int k = 20; // ans = 4
+        //int k = 2; // ans = -1
+        int k = 10; // ans = 2
+
+/* 
         // Approach-01
-        for(int i=arr.length-1;i>=0;i--)
+        for(int i=len-1;i>=0;i--)
         {
             if(arr[i]==k)
             {
@@ -18,6 +22,34 @@ public class IndexOfLastOccurance
             }
         }
         System.out.println(-1);
+*/
+
+        // Approach-02 Recursive Approach
+        int start = 0;
+        int end = len-1;
+        int reasult = search(arr,k,start,end);
+
+        System.out.println(reasult);
         
+    }
+
+    private static int search(int[] arr, int k, int start, int end) 
+    {
+        if(start>end)
+        return -1;
+
+        int mid = (start+end)/2;
+
+        if(arr[mid]>k)
+            return search(arr, k, start, mid-1);
+        else if(arr[mid]<k)   
+            return search(arr, k, mid+1, end); 
+        else
+            {
+                if(mid==0 || arr[mid]!=arr[mid+1])
+                    return mid;
+                else
+                    return search(arr, k, mid+1, end);
+            }    
     }    
 }
