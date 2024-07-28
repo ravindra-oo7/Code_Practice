2f4b114f5010f74e5cee9991c8a46fa59347a852
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-28 12:33:00 +0530

    Count Ones in Binary Array Approach-01 Binary Search Iterative Approach

--- GFGPractice/Searching/BinarySearch/CountOnesInBinaryArray.java
+++ GFGPractice/Searching/BinarySearch/CountOnesInBinaryArray.java
@@ -3,11 +3,11 @@ package Searching.BinarySearch;
 public class CountOnesInBinaryArray {
     public static void main(String[] args) 
     {
-        //int[] arr = {0,0,0,1,1,1,1}; 
-        int[] arr = {0,0,0,0};
+        int[] arr = {0,0,0,1,1,1,1}; 
+        //int[] arr = {0,0,0,0};
         int len = arr.length;
         
-        
+/*        
         // Approach-01
         int counter = 0;
         for(int i=0;i<len;i++)
@@ -18,7 +18,39 @@ public class CountOnesInBinaryArray {
             }
         }
         System.out.println("1 : "+counter);
-
-
+*/
+        // Approach-02 If Array is sorted
+        int start = 0;
+        int end = len-1;
+        
+       int lowInd =  getLowestIndexOfOne(arr,start,end);
+       if(lowInd!=-1)
+            System.out.println("Count : "+(len-lowInd));
+        else
+            System.out.println(0);
+        
+        
     }
+    
+    private static int getLowestIndexOfOne(int[] arr, int start, int end) 
+    {
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+        
+               if(arr[mid]<1)
+                   start=mid+1;
+               else
+              {
+                  if(arr[mid-1]!=1)
+                    return mid;
+                  else
+                  {
+                    end = mid-1;
+                  }  
+              }    
+         } 
+         return -1;
+    }
+    
 }
