494e3c611d264318d29cc3040dcc77995364dde2
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-28 15:58:58 +0530

    Find Square Root Approch-03 Binary Search Approach

--- GFGPractice/Searching/BinarySearch/FindSquareRoot.java
+++ GFGPractice/Searching/BinarySearch/FindSquareRoot.java
@@ -21,5 +21,38 @@ public class FindSquareRoot {
         }
         System.out.println(i-1);
 
+        // Approach-03
+         
+        int temp = sqrtFloor(numb);
+        System.out.println(temp);
+
+    }
+
+    private static int sqrtFloor(int numb)
+    {
+        int start = 1 ;
+        int end = numb;
+        int ans = -1;
+
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+            int midSqr = mid*mid;
+
+            if(midSqr<numb)
+            {
+                start = mid+1;
+                ans = mid;
+            }
+            else if(midSqr>numb)
+                end = mid-1;
+            else
+            {
+                return mid;
+            }    
+
+        }
+
+        return ans;
     }
 }
