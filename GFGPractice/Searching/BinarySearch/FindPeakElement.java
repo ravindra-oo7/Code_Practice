91e9087fc3953c5b4e7a4184317f7a534ab79b3e
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-29 17:50:09 +0530

    Find Peak Element in Array Approach-01 Binary search

--- GFGPractice/Searching/BinarySearch/FindPeakElement.java
+++ GFGPractice/Searching/BinarySearch/FindPeakElement.java
@@ -7,12 +7,18 @@ public class FindPeakElement {
         //int[] arr = {10,20,15,5,23,90,67}; // ans = 20
         //int[] arr = {80,70,60}; // ans = 80
         //int[] arr = {1,2,3,4}; // ans = 4
+        //int[] arr = {99};  // ans = 99
 
-
-        int len = arr.length;
+        int len = arr.length; 
         
-        // Approach-01 naive solution
-        if(arr[0]>arr[1])
+/*         
+        // Approach-01 naive solution 
+        if(len==1)
+        {
+            System.out.println(arr[len-1]);
+            return;
+        }
+        if(arr[0]>=arr[1])
         {
             System.out.println(arr[0]);
             return ;
@@ -20,21 +26,45 @@ public class FindPeakElement {
 
         for(int i=1; i<len-2;i++)
         {
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1])
+            if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1])
             {
                 System.out.println(arr[i]);
                 return ;
             }
         }
 
-        if(arr[len-1]>arr[len-2])
+        if(arr[len-1]>=arr[len-2])
         {
             System.out.println(arr[len-1]);
             return ;
         }
+*/
 
+        // Approach-02 Binary search method
 
-
+        int result = binarySearchForPeak(arr);
+        System.out.println(arr[result]);
 
     }
+
+    private static int binarySearchForPeak(int[] arr)
+    {
+        int start = 0;
+        int end = arr.length-1;
+        
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+            if( (mid==0 || arr[mid]>arr[mid-1]) &&
+                ((mid==arr.length-1) || arr[mid]>arr[mid+1]))
+                return mid;
+
+            if(mid>0 && arr[mid-1]>= arr[mid])
+                end=mid-1;
+            else
+                start = mid+1;    
+        }
+        return -1;
+    }
+
 }
