29e9eb447edd2634e64bd3451b660d9b355d417c
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-08-01 17:22:29 +0530

    Find Majority Element in Array Approach-02 Moore's Voting Algorithm

--- GFGPractice/Searching/MajorityElement/FindMajorityElementInArray.java
+++ GFGPractice/Searching/MajorityElement/FindMajorityElementInArray.java
@@ -5,10 +5,13 @@ public class FindMajorityElementInArray {
     {
         //int[] arr = {8,3,4,8,8}; // ans = 8
         //int[] arr = {20,30,40,50,50,50,50}; // ans= 50
-        int[] arr = {3,7,4,7,7,5}; // ans = No Majority
+        //int[] arr = {3,7,4,7,7,5}; // ans = No Majority
+        int[] arr = {2,3,2,4,2,5,2,6}; // ans = No Majority
+
 
         int len = arr.length;
         
+/*         
         // Approch-01
         for(int i=0;i<len;i++)
         {
@@ -16,9 +19,7 @@ public class FindMajorityElementInArray {
             for(int j=i;j<len;j++)
             {
                 if(arr[i]==arr[j])
-                {
                     count++;
-                }
             }
             if(count>len/2)
             {
@@ -27,5 +28,39 @@ public class FindMajorityElementInArray {
             }
         }
         System.out.println("No Majority!!!");
+*/
+
+        // Approch-02
+
+        //Finding a Candidate
+        int res = 0;
+        int count = 1;
+        for(int i=1;i<len;i++)
+        {
+            if(arr[res]==arr[i])
+                count++;
+            else
+                count--;
+            
+            if(count==0)
+            {
+                res = i;
+                count= 1;
+            }
+        }
+
+        count = 0;
+        for(int i=0;i<len;i++)
+        {
+            if(arr[res]==arr[i])
+                count++;    
+        }
+
+        if(count>len/2)
+            System.out.println(arr[res]);
+        else
+            System.out.println("No Majority!!!");
+
+        
     }
 }
