611f90f60b3c3bbf9c92cf76936d01d07270462c
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-08-02 16:52:37 +0530

    Find Reating Element in Array Approach-04 Eficiante solution Time = O(n) && Space = O(1)

--- GFGPractice/Searching/FindReapetingElementInArray/FindRepeatingElementInArray.java
+++ GFGPractice/Searching/FindReapetingElementInArray/FindRepeatingElementInArray.java
@@ -7,7 +7,13 @@ public class FindRepeatingElementInArray
 {
     public static void main(String[] args) 
     {
-        int[] arr = {0,2,1,3,2,2};   
+        //int[] arr = {0,2,1,3,2,2}; // ans = 2
+        //int[] arr = {1,3,2,4,3,3}; // ans = 3
+        int[] arr = {3,4,5,1,2,4,4}; // ans = 4 
+        //int[] arr = {1,1}; // ans = 1
+        //int[] arr = {0,0}; // ans = 0
+        //int[] arr = {1}; // ans = 1
+
         int len = arr.length;
 
 /*         
@@ -40,6 +46,7 @@ public class FindRepeatingElementInArray
         System.out.println("No Repeating Element Found!!!");
 */
 
+/* 
         // Approach-03 Efficiant Solution { Time = O(n) && Space = O(n) }
 
         boolean[] arrBool = new boolean[len];
@@ -57,6 +64,28 @@ public class FindRepeatingElementInArray
             }
         }
         System.out.println("No Repeating Element Found!!!");
+*/
+
+        // Approach-04 Efficiant Solution { Time = O(n) && Space = O(1) }
+        // Does not works for array with less than 2 elements
+        int slow = arr[0]+1;
+        int fast = arr[0]+1;
+
+        do 
+        {
+            slow = arr[slow]+1;
+            fast = arr[arr[fast]+1]+1;
+        } 
+        while(slow != fast);
+
+        slow = arr[0]+1;
+        while (slow != fast) 
+        {
+            fast = arr[fast]+1;
+            slow = arr[slow]+1;    
+        }
+
+        System.out.println(slow-1);
 
     }    
 }
