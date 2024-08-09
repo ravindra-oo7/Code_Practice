6ae9b340453308a4f1430b48062ebafc15447f41
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-08-09 12:18:04 +0530

    Arrays Sort Method Object Type

---
+++ GFGPractice/SortingAlgorithms/ArraysSortMethod/SortingNonPremitiveType/MainMethod.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class MainMethod
+{
+    public static void main(String[] args) 
+    {
+        
+        Point[] arr = {new Point(10,20),new Point(3,12), new Point(5,7)};
+        Arrays.sort(arr);
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i].getX()+" "+arr[i].getY());
+        }
+        
+    }
+}
