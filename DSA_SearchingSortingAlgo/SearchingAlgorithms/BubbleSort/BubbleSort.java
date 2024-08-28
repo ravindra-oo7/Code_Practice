91771d7d4008a037e3b133f6dd1c1ad6f4c98811
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-08-28 11:23:18 +0530

    Bubble Sort Implementation Approach-02 improvised

--- DSA_SearchingSortingAlgo/SearchingAlgorithms/BubbleSort/BubbleSort.java
+++ DSA_SearchingSortingAlgo/SearchingAlgorithms/BubbleSort/BubbleSort.java
@@ -8,11 +8,13 @@ public class BubbleSort {
         int[] arr = {36,19,29,12,5};
         System.out.println("Array: "+Arrays.toString(arr));
 
+/*         
         // My Approach-01
         for(int i=arr.length-1;i>0;i--)
         {
             for(int j=0;j<i;j++)
             {
+                System.out.print("   "+j);
                 //if(arr[j]>arr[j+1]) // Acending sort 
                 if(arr[j]<arr[j+1]) // Decending sort
                 {
@@ -20,9 +22,29 @@ public class BubbleSort {
                 }
             }
         }
+*/
+        
+        // Approach-02
+        for(int i=0;i<arr.length;i++)
+        {
+            int flag = 0;
+            for(int j=0;j<arr.length-1-i;j++)
+            {
+                System.out.print("   "+j);
+                //if(arr[j]>arr[j+1]) // Acending sort 
+                if(arr[j]<arr[j+1]) // Decending sort
+                {
+                    swap(arr,j);
+                    flag = 1;
+                }
+            }
+            if(flag == 0) //reduce extra iterations if array is already sorted
+            {
+                break;
+            }
+        }
+        
         System.out.println("Sorted Array : "+Arrays.toString(arr));
-
-
     }
 
     private static void swap(int[] arr, int j) 
