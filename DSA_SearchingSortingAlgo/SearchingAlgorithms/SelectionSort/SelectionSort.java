e26d62977fdaead376346a99abf58fa405da2fbe
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-08-28 15:15:37 +0530

    Selection Sort Approach-02

--- DSA_SearchingSortingAlgo/SearchingAlgorithms/SelectionSort/SelectionSort.java
+++ DSA_SearchingSortingAlgo/SearchingAlgorithms/SelectionSort/SelectionSort.java
@@ -8,16 +8,18 @@ public class SelectionSort {
         int[] arr = {38,52,9,18,6,62,13};
         //int[] arr = {1,8,3,4,5};
         System.out.println("Array : "+Arrays.toString(arr));
+        int temp ;
         
-        // Approach-01
+/*   
+      // Approach-01
             int counter = 0;
-            while(counter<arr.length) // 6 iterations
+            while(counter<arr.length) 
             {   
                 for(int j=counter+1;j<arr.length;j++)
                 {
                     if(arr[counter] > arr[j])
                     {
-                        int temp = arr[j];
+                        temp = arr[j];
                         arr[j] = arr[counter];
                         arr[counter] = temp; 
                     }
@@ -26,13 +28,29 @@ public class SelectionSort {
                 counter++;
                 
             }
+*/
+
+        // Approach-02
+
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int min = i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[min]>arr[j])
+                {
+                    min = j;
+                }
+            }
+            temp = arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+            
+        }
+
+
 
             System.out.println("Sorted Array:"+Arrays.toString(arr));
-   
-
-        
-
-
 
     }
 }
